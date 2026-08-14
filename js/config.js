cd937ab41c3980bbdec6636412bd38565d3831bb
krad1985 <krad1985@gmail.com>
2026-08-14 10:38:28 +0800

    Fix GAS manifest: add webapp settings; update config.js to v5 deployment

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // 前端設定 - 請勿直接修改此檔案，部署時由建置流程注入
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxVix9grUmXq-BMy1Y9LhvZWMiVyqZATaVHW7zwlPP1D6Cu32HW2GoutLdDELn-5Bv9Dg/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwGGsoZrmc7dGyYxBbMr1p6igTR5GCFf36sbOkf2lzCrLjlfVWbUZAHDr2fNGUtIW4igg/exec',
   // PUBLIC_TOKEN 會在首次載入活動列表時從 GAS 取得，並儲存於 sessionStorage
 };
